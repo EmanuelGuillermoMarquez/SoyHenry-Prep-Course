5f4f5efd63e2b80c16c2f8ffad80adbc03662f4e
EmanuelGuillermoMarquez <tu@email.commarquezema127@gmail.com>
2022-07-29 17:57:24 -0300

    JSX_Homework_Finished

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  var nuevoArray = [];
+  for (var clave in objeto) {
+    nuevoArray.push([clave, objeto[clave]]);
+  }
+  return nuevoArray;
 }
 
 
@@ -18,6 +23,17 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var newObject = {};
+  var contador = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (!newObject[string[i]]) {
+      newObject[string[i]] = 1;
+    }
+    else {
+      newObject[string[i]] = newObject[string[i]] + 1;
+    }
+  }
+  return newObject;
 }
 
 
@@ -26,6 +42,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayusculaString = "";
+  var minusculaString = "";
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === s[i].toUpperCase()) {
+      mayusculaString = mayusculaString + s[i];
+    }
+    else if (s[i] === s[i].toLowerCase()) {
+      minusculaString = minusculaString + s[i];
+    }
+    var nuevaString = mayusculaString.concat(minusculaString)
+  }
+  return nuevaString;
 }
 
 
@@ -35,6 +63,10 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var newStrInvert = str.split(" ").map(function(elemento){
+    return elemento.split("").reverse().join("");
+  })
+  return newStrInvert.join(" "); 
 } 
 
 
@@ -43,13 +75,35 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  if (numero.toString() === numero.toString().split("").reverse().join("")) {
+    return "Es capicua";
+  }
+  else {
+    return "No es capicua"
+  }
 }
+//Otra opción:
+//var numeroString = numero.toString();
+//var numeroInvert = numeroString.split("").reverse().join("");
+//if (numeroString === numeroInvert) {
+//  return "Es capicua";
+//  }
+//  else {
+//    return "No es capicua"
+//  }
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var nuevaCadena = "";
+  for (var i = 0; i < cadena.length; i ++) {
+    if (cadena[i] !== "a" && cadena[i] !== "b" && cadena[i] !== "c") {
+      nuevaCadena = nuevaCadena + cadena[i];
+    }    
+  }
+  return nuevaCadena;
 }
 
 
@@ -57,7 +111,17 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  for (var i = 0; i < arr. length-1; i ++) {
+    while (arr[i].length > arr[i+1].length) {
+      var ord = arr[i];
+      arr[i] = arr[i+1];
+      arr[i+1] = ord;
+      i--;
+    }
+  }
+  return arr;
 }
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -66,6 +130,15 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var nuevoArreglo = [];
+  for (var i = 0; i < arreglo1.length; i++) {
+    for (var x = 0; x < arreglo2.length; x++) {
+      if (arreglo1[i] === arreglo2[x]) {
+        nuevoArreglo.push(arreglo1[i]);
+      }
+    }
+  }
+  return nuevoArreglo;
 }
 
 
